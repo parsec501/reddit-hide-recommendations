d8f1a626fd46520cd897c1a329c1d521983a213d
Moritz <105080989+parsec501@users.noreply.github.com>
2023-10-05 00:19:03 +0200

    Added some metadata

--- userscript.js
+++ userscript.js
@@ -1,16 +1,19 @@
 // ==UserScript==
 // @name     Hide recommendations on Reddit
+// @name:de Empfehlungen auf Reddit ausblenden
 // @namespace HideRecommendationsReddit
 // @description Removes all recommended posts from communities you aren't subscribed to from your Reddit timeline.
+// @description:de Entfernt alle empfohlenen Beiträge von Communities, die man nicht abonniert hat, aus der Reddit-Timeline.
 // @match  https://www.reddit.com/
 // @match  https://www.reddit.com/r/*
 // @version  1.0
+// @license Unlicense
 // @grant    none
 // @run-at document-start
 // ==/UserScript==
-
+ 
 let x = 0;
-
+ 
 function hide_recommendations() {
   // Define the target strings to match
   const targetStrings = [
@@ -19,14 +22,14 @@ function hide_recommendations() {
     "Popular near you",
     "Suggested",
   ];
-
+ 
   // Find all elements with the data-testid attribute
   const elements = document.querySelectorAll('[data-testid="post-container"]');
   // Loop through the elements
   for (let element of elements) {
     // Find the <p> element containing the text
     const matchingPs = element.querySelectorAll('p');
-
+ 
     for (let p of matchingPs) {
       for (let targetString of targetStrings) {
         if (p.textContent.includes(targetString)) {
@@ -39,7 +42,7 @@ function hide_recommendations() {
     }
   }
 };
-
+ 
 hide_recommendations();
 //Also rerun the code each time document change (i.e new posts are added when user scroll down)
 document.addEventListener("DOMNodeInserted", hide_recommendations);
